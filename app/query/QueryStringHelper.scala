5a4d9e6f92d4bd9be3a372f2c3bab1b6bf184e6e
Radu Crican <radu.crican@workday.com>
2018-11-05 15:02:39 +0100

    BatchSupport - added query parameter parsing, test and first implementation

--- app/query/QueryStringHelper.scala
+++ app/query/QueryStringHelper.scala
@@ -5,31 +5,36 @@ import models.RatingInfosRequest
 import scala.util.Try
 
 object QueryStringHelper {
-  def parseQueryParams(queryParams: String): RatingInfosRequest = {
-    queryParams.replace("?", "")
-    val maybeParamsStr = Try(queryParams.split("&")).toOption
-    maybeParamsStr.map { paramStr =>
+  def parseQueryParams(queryParams: String): Option[RatingInfosRequest] = {
+    val filteredQueryParams = queryParams.replace("?", "")
+    val maybeParamsStr = Try(filteredQueryParams.split("&")).toOption
+    maybeParamsStr.flatMap { paramStr =>
       val maybeMovieIdsStr = paramStr.headOption
-      val maybeMovieIds = maybeMovieIdsStr.flatMap { movieIdsStr =>
+      maybeMovieIdsStr.flatMap { movieIdsStr =>
         val maybeMovieIdsParams = Try(movieIdsStr.split("=")).toOption
-        maybeMovieIdsParams.map{movieIdsParams =>
+        maybeMovieIdsParams.flatMap { movieIdsParams =>
           val movieIdsStr = movieIdsParams.headOption
           if (movieIdsStr.exists(paramName => paramName.equals("movieIds"))) {
             val maybeMovieIds = Try(movieIdsParams(1).split(",")).toOption.map(_.map(_.toInt))
             val maybeLimitParam = Try(paramStr(1)).toOption
-            maybeLimitParam.map { limitParam =>
+            maybeLimitParam.flatMap { limitParam =>
               val maybeLimitParams = Try(limitParam.split('=')).toOption
-              maybeLimitParams.flatMap(limitParams => limitParams.headOption)
+              maybeLimitParams.flatMap { limitParams =>
+                val limitParamKey = limitParams.headOption
+                if (limitParamKey.exists(paramName => paramName.equals("limit"))) {
+                  val maybeLimitParamValue = Try(limitParams(1).toInt).toOption
+                  for {
+                    movieIds <- maybeMovieIds
+                    limitParamValue <- maybeLimitParamValue
+                  } yield RatingInfosRequest(movieIds.toSeq, limitParamValue)
+                }
+                maybeMovieIds.map(movieIds => RatingInfosRequest(movieIds.toSeq, 5))
+              }
             }
           }
-
+          else None
         }
       }
-//        Try(movieIdsStr.split(",").map(_.toInt)).toOption)
-      val maybeLimitStr = Try(paramStr(1)).toOption
-      maybeLimitStr.map(limitStr => limitStr)
     }
-
-    ???
   }
 }
--- test/query/QueryStringHelperTest.scala
+++ test/query/QueryStringHelperTest.scala
@@ -6,9 +6,9 @@ import org.scalatest.{MustMatchers, WordSpec}
 class QueryStringHelperTest extends WordSpec with MustMatchers {
   "The QueryStringHelper" when {
     "calling parseQueryParams" should {
-      "return a valid RatingInfosRequest" ignore {
+      "return a valid RatingInfosRequest" in {
         val queryParams = "?movieIds=680,43,23&limit=5"
-        QueryStringHelper.parseQueryParams(queryParams) mustBe RatingInfosRequest(Seq(680, 42,23), 5)
+        QueryStringHelper.parseQueryParams(queryParams) mustBe Some(RatingInfosRequest(Seq(680, 43,23), 5))
       }
     }
   }
